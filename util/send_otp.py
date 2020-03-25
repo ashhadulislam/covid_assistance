994ac226c4a0d77504ee68ab80d6309ee31769a0
abdullah4vr <zeesh2142@gmail.com>
2020-03-25 14:03:19 +0530

    Added security details to environment variables

--- util/send_otp.py
+++ util/send_otp.py
@@ -1,17 +1,18 @@
 from twilio.rest import Client
+from os import environ
 from otp_gen import get_otp 
 
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
 def send_otp_sms(user_phone):
- account_sid = 'ACa0b413564f3a42afaa1551c7f72f8165'
- auth_token = '826eb4d0525ac467e790f3ea3e872e87'
+ account_sid = environ.get('account_sid')
+ auth_token =  environ.get('auth_token')
  client = Client(account_sid, auth_token)
  user_otp = get_otp(user_phone)
  message = client.messages.create(
          body   =  'Your OTP for Covid Help is :'+str(user_otp),
-         from_  =  '+12058465187',
+         from_  =  environ.get('reg_phone'),
          to     =  str(user_phone),
      )
 
