693713c81bb9002b4e2344da378091e6d6d21795
jmishra <jmishra@evertz.com>
2022-03-14 18:34:08 +0530

    update in entry

--- util/check.py
+++ util/check.py
@@ -1,7 +1,7 @@
 from send_otp import send_otp_sms
 from validate_phone import validate_phone
 
-phone = '+919804002142'
+phone = '+918777061118'
 send_otp_sms(phone)
 otp =input('Enter otp :\n')
 if(validate_phone(phone,otp)):
